5ef5ed0a8a99b5b4b93e351b288afc20d93b6ec8
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-04 00:22:11 -0400

    11

--- Frontend/src/form.jsx
+++ Frontend/src/form.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export function FormPage() { 
     return (
-        <div className="size-full bg-[url('/snowy-village-5120x2880-20406 (1).jpg')] bg-no-repeat bg-cover">
+        <div className="size-full bg-[url('/snowy-village-5120x2880-20406.jpg')] bg-no-repeat bg-cover flex justify-center items-center">
             <div className="h-[90%] w-[45%] bg-slate-950/20 flex flex-col justify-center items-center gap-5">
                 <h1>Hello, World!</h1>
                 <h2>Welcome to the Form Page</h2>
