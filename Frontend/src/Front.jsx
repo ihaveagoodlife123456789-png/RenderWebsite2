fa87a030b50061a3b6119af3396e664fd8766910
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-04 19:22:09 -0400

    15

--- Frontend/src/Front.jsx
+++ Frontend/src/Front.jsx
@@ -6,7 +6,7 @@ export function FrontPage() {
 
   return (
     <div className="size-full text-[35px] bg-[url('/christmas-tree-3840x2160-14452.jpg')] bg-no-repeat bg-cover flex justify-center items-center gap-12 font-semibold text-slate-200">
-      <div className="w-full h-[25%] bg-slate-950/20 flex justify-center items-center gap-5">
+      <div className="w-full h-[25%] bg-slate-950/20 flex justify-center items-center gap-20">
         <Link to='/form'><motion.h2
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
