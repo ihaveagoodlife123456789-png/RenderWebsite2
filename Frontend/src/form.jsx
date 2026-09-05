3c637768b4b5c9b0a705662b9c34132bf88cf5b3
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-04 23:05:35 -0400

    34

--- Frontend/src/form.jsx
+++ Frontend/src/form.jsx
@@ -7,7 +7,7 @@ export function FormPage() {
             <div className="relative h-[90%] w-[45%] bg-slate-950/20 flex flex-col justify-center items-center gap-5">
             <h1 className="text-white font-semibold text-[45px]">Create a Message</h1>
             <p className="w-[55%] wrap-break-word text-slate-300/90 font-semibold text-[14px]">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
-            <motion.form className="flex flex-col align-items justify-content gap-5 font-semibold text-slate-200" >
+            <motion className="flex flex-col align-items justify-content gap-5 font-semibold text-slate-200" >
                 <h3>Name</h3>
                 <motion.input placeholder="Enter your name"></motion.input>
                 <h3>Message</h3>
@@ -16,9 +16,9 @@ export function FormPage() {
                 <motion.input placeholder="Enter message color"></motion.input>
                 <h3>Email</h3>
                 <motion.input placeholder="Enter your email"></motion.input>
-                <motion.button className="size-fit text-slate-200 bg-blue-800 font-semibold text-[22px] rounded-[15px] py-[6px] px-[10px]" initial={{scale: 0, y: -20}} animate={{scale: 1, y: 0}} transition={{duration: .2}} onHover={{scale: 1.4}}>Submit</motion.button>
-            </motion.form>
-                <Link to='/'><motion.div className="text-white font-semibold absolute top-5 left-5 bg-blue-900/75 size-fit py-[4px] px-[2px] rounded-[10px]" initial={{scale: 0, y: -20}} animate={{scale: 1, y: 0}} transition={{duration: .2}} onHover={{scale: 1.4}}>Go home</motion.div></Link>
+                <motion.button className="size-fit text-slate-200 bg-blue-800 font-semibold text-[22px] rounded-[15px] py-[6px] px-[10px]" initial={{scale: 0}} onHover={{scale: 1.04}}>Submit</motion.button>
+            </motion>
+                <Link to='/'><motion.div className="text-white font-semibold absolute top-5 left-5 bg-blue-900/75 size-fit py-[4px] px-[2px] rounded-[10px]" initial={{scale: 0, y: -20}} animate={{scale: 1, y: 0}} transition={{duration: .2}} onHover={{scale: 1.04}}>Go home</motion.div></Link>
             </div>
             <img src="/icons8-wreath-64.png" className="size-fit absolute top-10 left-10"/>
         </div>
