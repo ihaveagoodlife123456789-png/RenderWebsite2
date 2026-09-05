be7b681a9455e615e42ee4289a3b3c9d6c059476
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-04 23:58:47 -0400

    45

--- Frontend/src/Front.jsx
+++ Frontend/src/Front.jsx
@@ -5,20 +5,23 @@ import { motion } from 'motion/react';
 export function FrontPage() {
 
   return (
-    <div className="size-full text-[35px] bg-[url('/christmas-tree-3840x2160-14452.jpg')] bg-no-repeat bg-cover flex justify-center items-center gap-12 font-semibold text-slate-200">
-      <div className="w-full h-[25%] bg-slate-950/20 flex justify-center items-center gap-20">
+    <div className="size-full text-[35px] bg-[url('/santa-aesthetic-5120x3413-13338.jpg')] bg-no-repeat bg-cover flex justify-center items-center gap-12 font-semibold text-slate-200">
+      <div className="relative w-full h-[25%] bg-slate-950/20 flex justify-center items-center gap-20">
         <Link to='/form'><motion.h2
+        className="font-semibold text-slate-300/90"
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
-        onHover={{ scale: 1.05}}
+        whileHover={{ scale: 1.05}}
         >Write a Message</motion.h2></Link>
         <Link to='/users'><motion.h2
+        className="font-semibold text-slate-300/90"
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
-        onHover={{ scale: 1.05}}
+        whileHover={{ scale: 1.05}}
         >View Users</motion.h2></Link>
+        <img src="/icons8-wreath-64.png" className="absolute top-12 left-12"/>
       </div>
     </div>
   )
