5ecad163b8bc25ddf9abbc00bde8d21d817e843f
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-04 22:48:10 -0400

    31

--- Frontend/src/form.jsx
+++ Frontend/src/form.jsx
@@ -6,7 +6,7 @@ export function FormPage() {
         <div className="size-full bg-[url('/snowy-village-5120x2880-20406.jpg')] bg-no-repeat bg-cover flex justify-center items-center" initial={{scale: 0, y: -20}} animate={{scale: 1, y: 0}} transition={{duration: .2}} onHover={{scale: 1.4}}>
             <div className="relative h-[90%] w-[45%] bg-slate-950/20 flex flex-col justify-center items-center gap-5">
             <h1 className="text-white font-semibold text-[45px]">Create a Message</h1>
-            <p className="w-[55%] wrap-break-word text-slate-300/90 font-semibold text-[14px]">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. <br/> Aenean commodo ligula eget dolor. <br/>Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. <br/>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+            <p className="w-[55%] wrap-break-word text-slate-300/90 font-semibold text-[14px]">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
             <motion.form className="flex flex-col align-items justify-content gap-5 font-semibold text-slate-200" >
                 <h3>Name</h3>
                 <motion.input placeholder="Enter your name"></motion.input>
