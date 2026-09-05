d990c56b2c1f7ad6bf58e53e26465758526d8a1b
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-05 00:23:21 -0400

    46

--- Frontend/src/Front.jsx
+++ Frontend/src/Front.jsx
@@ -8,21 +8,21 @@ export function FrontPage() {
     <div className="size-full text-[35px] bg-[url('/santa-aesthetic-5120x3413-13338.jpg')] bg-no-repeat bg-cover flex justify-center items-center gap-12 font-semibold text-slate-200">
       <div className="relative w-full h-[25%] bg-slate-950/20 flex justify-center items-center gap-20">
         <Link to='/form'><motion.h2
-        className="font-semibold text-slate-300/90"
+        className="font-bold text-slate-300/90"
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        whileHover={{ scale: 1.05}}
+        transition={{ duration: 0.2 }}
+        whileHover={{ scale: 1.08}}
         >Write a Message</motion.h2></Link>
         <Link to='/users'><motion.h2
-        className="font-semibold text-slate-300/90"
+        className="font-bold text-slate-300/90"
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        whileHover={{ scale: 1.05}}
+        transition={{ duration: 0.2 }}
+        whileHover={{ scale: 1.08}}
         >View Users</motion.h2></Link>
-        <img src="/icons8-wreath-64.png" className="absolute top-12 left-12"/>
       </div>
+      <img src="/icons8-wreath-64.png" className="absolute top-12 left-12"/>
     </div>
   )
 }
