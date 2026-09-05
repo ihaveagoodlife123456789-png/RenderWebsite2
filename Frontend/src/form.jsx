f1b37d1e52e48252042eddfcf1072d6ae8a6212e
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-04 23:37:39 -0400

    40

--- Frontend/src/form.jsx
+++ Frontend/src/form.jsx
@@ -7,18 +7,18 @@ export function FormPage() {
             <div className="relative h-[90%] w-[45%] bg-slate-950/20 flex flex-col justify-center items-center gap-5">
             <h1 className="text-white font-semibold text-[45px]">Create a Message</h1>
             <p className="w-[55%] wrap-break-word text-slate-300/90 font-semibold text-[14px]">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
-            <motion.form  className="flex flex-col align-items justify-content gap-5 font-semibold text-slate-200" >
+            <motion.form  className="flex flex-col align-items justify-content gap-5 font-semibold text-slate-200" initial={{scale: 0, y: -30}} animate={{scale: 1}} transition={{duration: .3}} whileHover={{scale: 1.06}}>
                 <h3>Name</h3>
-                <motion.input placeholder="Enter your name"></motion.input>
+                <input placeholder="Enter your name"></input>
                 <h3>Message</h3>
-                <motion.input placeholder="Enter your message"></motion.input>
+                <input placeholder="Enter your message"></input>
                 <h3>Message color</h3>
-                <motion.input placeholder="Enter message color"></motion.input>
+                <input placeholder="Enter message color"></input>
                 <h3>Email</h3>
-                <motion.input placeholder="Enter your email"></motion.input>
+                <input placeholder="Enter your email"></input>
                 <motion.button className="size-fit text-slate-200 bg-blue-800 font-semibold text-[22px] rounded-[15px] py-[6px] px-[10px]" initial={{scale: 0, y: -20}} animate={{scale: 1, y: 0}} transition={{duration: .2}} whileHover={{scale: 1.1}}>Submit</motion.button>
             </motion.form>
-                <Link to='/'><motion.div className="text-white text-[22px] font-semibold absolute top-5 left-5 bg-blue-900/75 size-fit py-[4px] px-[2px] rounded-[6px]" initial={{scale: 0, y: -20}} animate={{scale: 1, y: 0}} transition={{duration: .1}} whileHover={{scale: 1.06}}>Go home</motion.div></Link>
+                <Link to='/'><motion.div className="text-white text-[22px] font-semibold absolute top-5 left-5 bg-blue-900/75 size-fit py-[4px] px-[2px] rounded-[6px]" initial={{scale: 0, y: -20}} animate={{scale: 1}} transition={{duration: .1}} whileHover={{scale: 1.06}}>Go home</motion.div></Link>
             </div>
             <img src="/icons8-wreath-64.png" className="size-fit absolute top-14 left-14"/>
         </div>
