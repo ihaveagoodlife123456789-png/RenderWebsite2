7a3bb56a45b728d031749f15006c91c89befad86
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-05 16:21:38 -0400

    83

--- Frontend/src/users.jsx
+++ Frontend/src/users.jsx
@@ -23,7 +23,7 @@ export function UsersPage() {
     return (
         <div className="size-full flex items-center justify-center bg-[url('/snowy-village-5120x2880-20406.jpg')] bg-no-repeat bg-cover">
           <div className="w-[68%] h-[70%] flex flex-col items-center justify-center">
-            <div className="relative h-[90%] w-[65%] bg-slate-950/20 flex flex-col justify-center items-center gap-5 font-semibold text-slate-200 overflow-scroll scrollbar-none">
+            <div className="relative h-[70%] w-[65%] bg-slate-950/20 flex flex-col justify-center items-center gap-5 font-semibold text-slate-200 overflow-scroll scrollbar-none">
                 {userData && userData.map(users => {
                     return (
                         <div key={users.id} className="flex justify-center items-center text-semibold gap-5 text-[18px]">
@@ -33,6 +33,7 @@ export function UsersPage() {
                         </div>
                     )
                 })}
+                <h1 className="absolute top-8">Messages</h1>
                 <Link to='/' className="absolute bottom-12"><motion.h2 className="bg-blue-400/20 border-[2px] border-blue-700 text-[22px] font-bold py-[6px] px-[8px] rounded-[12px]" initial={{scale: 1}} whileHover={{scale: 1.08}}>Go back</motion.h2></Link>
             </div>
             </div>
