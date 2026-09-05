12142c6f2a01312a458f537db729f04c4cdc1b29
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-05 01:20:38 -0400

    52

--- Frontend/src/users.jsx
+++ Frontend/src/users.jsx
@@ -24,14 +24,14 @@ export function UsersPage() {
             <div className="h-[90%] w-[65%] bg-slate-950/20 flex flex-col justify-center items-center gap-5 font-semibold text-slate-200 overflow-scroll scrollbar-none">
                 {userData && userData.map(users => {
                     return (
-                        <div key={users.id} className="flex justify-center items-center text-semibold gap-5">
+                        <div key={users.id} className="flex justify-center items-center text-semibold gap-5 text-[18px]">
                             <h3>{users.id}</h3>
                             <h3>{users.name}</h3>
                             <p>{users.message}</p>
                         </div>
                     )
                 })}
-                <Link to='/'><motion.h2 className="bg-blue-400/20 border-[2px] border-blue-700 text-[22px] font-semibold py-[6px] px-[8px]" initial={{scale: 1}} whileHover={{scale: 1.08}}>Go back</motion.h2></Link>
+                <Link to='/'><motion.h2 className="bg-blue-400/20 border-[2px] border-blue-700 text-[22px] font-bold py-[6px] px-[8px] rounded-[12px]" initial={{scale: 1}} whileHover={{scale: 1.08}}>Go back</motion.h2></Link>
             </div>
         </div>
     )
