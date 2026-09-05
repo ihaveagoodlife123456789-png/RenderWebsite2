b2de5912fae094f24a3c4a99f21e15e958a9230c
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-04 23:53:07 -0400

    44

--- Frontend/src/form.jsx
+++ Frontend/src/form.jsx
@@ -4,7 +4,7 @@ import { motion } from 'motion/react';
 export function FormPage() { 
     return (
         <div className="size-full bg-[url('/snowy-village-5120x2880-20406.jpg')] bg-no-repeat bg-cover flex justify-center items-center" initial={{scale: 0, y: -20}} animate={{scale: 1, y: 0}} transition={{duration: .1}}>
-            <motion.div className="relative h-[90%] w-[45%] bg-slate-950/20 flex flex-col justify-center items-center gap-5" initial={{scale: 0, y: -30}} animate={{scale: 1}} transition={{duration: .3}} whileHover={{scale: 1.06}}>
+            <motion.div className="relative h-[90%] w-[45%] bg-slate-950/20 flex flex-col justify-center items-center gap-5" initial={{scale: 0, y: -30}} animate={{scale: 1}} transition={{duration: .3}}>
             <h1 className="text-white font-semibold text-[45px]">Create a Message</h1>
             <p className="w-[55%] wrap-break-word text-slate-300/90 font-semibold text-[14px]">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
             <motion.form  className="flex flex-col align-items justify-content gap-5 font-semibold text-slate-200">
@@ -18,7 +18,7 @@ export function FormPage() {
                 <input placeholder="Enter your email"></input>
                 <motion.button className="size-fit text-slate-200 bg-blue-800 font-semibold text-[22px] rounded-[15px] py-[6px] px-[10px]" initial={{scale: 0, y: -20}} animate={{scale: 1, y: 0}} transition={{duration: .2}} whileHover={{scale: 1.1}}>Submit</motion.button>
             </motion.form>
-                <Link to='/'><motion.div className="text-white text-[22px] font-semibold absolute top-5 left-5 bg-blue-900/75 size-fit py-[4px] px-[2px] rounded-[6px]" initial={{scale: 0, y: -20}} animate={{scale: 1}} transition={{duration: .1}} whileHover={{scale: 1.06}}>Go home</motion.div></Link>
+                <Link to='/'><motion.div className="text-white text-[16px] font-semibold absolute top-8 left-8 bg-blue-900/75 size-fit py-[4px] px-[2px] rounded-[6px]" initial={{scale: 0, y: -20}} animate={{scale: 1}} transition={{duration: .1}} whileHover={{scale: 1.06}}>Go home</motion.div></Link>
             </motion.div>
             <img src="/icons8-wreath-64.png" className="size-fit absolute top-14 left-14"/>
         </div>
