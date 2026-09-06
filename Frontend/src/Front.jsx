73c70b6a7fc78b2cc7535452ce60ef6b613cfcdf
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-06 17:40:57 -0400

    140

--- Frontend/src/Front.jsx
+++ Frontend/src/Front.jsx
@@ -7,13 +7,13 @@ export function FrontPage() {
   return (
     <div className="size-full text-[35px] bg-[url('/winter-mountain-5120x2880-24311.jpg')] bg-no-repeat bg-cover bg-center flex flex-col text-slate-200">
       <div className="absolute top-15 w-[62%] h-[8%] left-[20%] bg-blue-200 rounded-[12px]"></div>
-      <div className="absolute top-20 right-[10%] w-[5%] h-[5%] bg-blue-500 rounded-[13px]"></div>
+      <div className="absolute top-20 right-[10%] w-[5%] h-[5%] bg-blue-500 rounded-[13px] text-center"></div>
       <div className="w-full h-[54%] flex flex-col justify-center items-center gap-8">
         <h1 className="font-bold text-[48px] text-orange-400/90">Ascended Horizons</h1>
         <h4 className="text-[24px] leading-[42px] font-semibold text-slate-950">Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. <br />Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. <br />Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</h4>
+        <Link to='/form'><motion.h2 className="font-bold text-blue-600/90 bg-blue-600/80 border-blue-950/90 rounded-[15px]" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>Write a Message</motion.h2></Link>
       </div>
       <div className="absolute bottom-0 w-full h-[46%] bg-slate-950/30 flex justify-center items-center gap-20 border-slate-400 border-[1px]">
-        <Link to='/form'><motion.h2 className="font-bold text-blue-600/90" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>Write a Message</motion.h2></Link>
         <Link to='/users'><motion.h2 className="font-bold text-blue-600/90" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>View Users</motion.h2></Link>
       </div>
       <img src="/icons8-wreath-64.png" className="absolute top-12 left-12"/>
