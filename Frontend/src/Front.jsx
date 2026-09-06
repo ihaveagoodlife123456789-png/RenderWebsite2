9ee1b841f5fee42b67952025a9dfbd7ca99ab7c2
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-06 16:47:02 -0400

    131

--- Frontend/src/Front.jsx
+++ Frontend/src/Front.jsx
@@ -6,7 +6,11 @@ export function FrontPage() {
 
   return (
     <div className="size-full text-[35px] bg-[url('/winter-mountain-5120x2880-24311.jpg')] bg-no-repeat bg-cover bg-center flex justify-center items-center gap-12 font-semibold text-slate-200">
-      <div className="relative w-full h-[25%] bg-slate-950/20 flex justify-center items-center gap-20 border-slate-400 border-[1px]">
+      <div className="size-full flex flex-col justify-center items-center gap-8">
+        <h1 className="font-bold text-[38px]">Ascended Horizons</h1>
+        <h4 className="font-[24px] font-semibold">Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. <br />Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. <br />Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</h4>
+      </div>
+      <div className="absolute bottom-0 w-full h-[48%] bg-slate-950/30 flex justify-center items-center gap-20 border-slate-400 border-[1px]">
         <Link to='/form'><motion.h2 className="font-bold text-blue-600/90" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>Write a Message</motion.h2></Link>
         <Link to='/users'><motion.h2 className="font-bold text-blue-600/90" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>View Users</motion.h2></Link>
       </div>
