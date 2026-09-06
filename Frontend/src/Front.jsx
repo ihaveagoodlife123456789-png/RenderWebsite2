81205155ab16947b30504c13f80cc13acb77a340
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-06 16:01:17 -0400

    128

--- Frontend/src/Front.jsx
+++ Frontend/src/Front.jsx
@@ -7,8 +7,8 @@ export function FrontPage() {
   return (
     <div className="size-full text-[35px] bg-[url('/alps-snow-mountains-3840x2160-25451.jpg')] bg-no-repeat bg-cover bg-center flex justify-center items-center gap-12 font-semibold text-slate-200">
       <div className="relative w-full h-[25%] bg-slate-950/20 flex justify-center items-center gap-20 border-slate-400 border-[1px]">
-        <Link to='/form'><motion.h2 className="font-bold text-slate-300/90" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>Write a Message</motion.h2></Link>
-        <Link to='/users'><motion.h2 className="font-bold text-slate-300/90" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>View Users</motion.h2></Link>
+        <Link to='/form'><motion.h2 className="font-bold text-cyan-500/90" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>Write a Message</motion.h2></Link>
+        <Link to='/users'><motion.h2 className="font-bold text-cyan-500/90" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>View Users</motion.h2></Link>
       </div>
       <img src="/icons8-wreath-64.png" className="absolute top-12 left-12"/>
     </div>
