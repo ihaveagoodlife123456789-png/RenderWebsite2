74379940148fb954f3a8555e81b294cada309e61
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-06 18:10:06 -0400

    146

--- Frontend/src/Front.jsx
+++ Frontend/src/Front.jsx
@@ -11,12 +11,12 @@ export function FrontPage() {
       <div className="w-full h-[68%] flex flex-col justify-center items-center gap-14">
         <h1 className="font-bold text-[48px] text-orange-400/90">Ascended Horizons</h1>
         <h4 className="text-[24px] leading-[42px] font-semibold text-slate-950">Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. <br />Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. <br />Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</h4>
-        <Link to='/form'><motion.h2 className="font-semibold text-[24px] py-12 px-18 text-white bg-blue-600/80 border-blue-800 border-[3px] rounded-[15px]" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>Write a Message</motion.h2></Link>
+        <Link to='/form'><motion.h2 className="font-semibold text-[24px] py-4 px-6 text-white bg-blue-600/80 border-blue-800 border-[3px] rounded-[15px]" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>Write a Message</motion.h2></Link>
       </div>
       <div className="absolute bottom-0 w-full h-[32%] bg-slate-950/30 flex justify-center items-center gap-20 border-slate-400 border-[1px]">
         <Link to='/users'><motion.h2 className="font-semibold text-[24px] text-blue-600/90" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>View Users</motion.h2></Link>
       </div>
-      <img src="/icons8-wreath-64.png" className="absolute top-12 left-12"/>
+      <img src="/icons8-wreath-64.png" className="absolute top-16 left-16"/>
     </div>
   )
 }
