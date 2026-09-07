12ff667706945700e1812c63caa429ea0e7f9b79
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-07 13:45:25 -0400

    245

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -7,7 +7,7 @@ export function FullPage() {
     return (
         <div className="h-full w-full overflow-y-scroll snap-y snap-mandatory">
         <FrontPage />
-        <motion.div className="size-full bg-slate-950/30 flex justify-center items-center snap-start">
+        <motion.div className="size-full bg-slate-950/30 flex justify-center items-center snap-start" dragMomentum={true}>
         <Link to='/users'><motion.h2 className="font-semibold text-[24px] text-blue-600/90" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>View Users</motion.h2></Link>
         </motion.div>
         </div>
