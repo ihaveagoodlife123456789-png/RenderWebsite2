b41209d3fb975f70bd79d3b01179fb0a2bd72b1c
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-07 16:15:25 -0400

    251

--- Frontend/src/bottom.jsx
+++ Frontend/src/bottom.jsx
@@ -11,16 +11,7 @@ export function BottomPage() {
         <h2 className="font-bold text-[34px] text-white">View users</h2>
         </motion.h2>
         <motion.div className="w-full h-[20%] bg-slate-950 relative bottom-0">
-
         </motion.div>
-        </Link>
-        <Link to='/users' className="w-[20%] h-[48%] bg-mist-800/60 font-semibold text-[24px] text-blue-600/90 flex flex-col justify-center items-start">
-        <motion.h2 className="w-full h-[65%] flex justify-center items-center bg-[url('/yosemite-lake-yosemite-valley-landscape-reflection-yosemite-6288x4192-1023.jpg')] bg-no-repeat bg-cover">
-        <h2 className="font-bold text-[34px] text-white">View users</h2></motion.h2><motion.div className="w-full h-[20%] bg-slate-950 relative bottom-0"></motion.div>
-        </Link>
-        <Link to='/users' className="w-[20%] h-[48%] bg-mist-800/60 font-semibold text-[24px] text-blue-600/90 flex flex-col justify-center items-start">
-        <motion.h2 className="w-full h-[65%] flex justify-center items-center bg-[url('/yosemite-lake-yosemite-valley-landscape-reflection-yosemite-6288x4192-1023.jpg')] bg-no-repeat bg-cover">
-        <h2 className="font-bold text-[34px] text-white">View users</h2></motion.h2><motion.div className="w-full h-[20%] bg-slate-950 relative bottom-0"></motion.div>
         </Link>
         </motion.div>
     )
