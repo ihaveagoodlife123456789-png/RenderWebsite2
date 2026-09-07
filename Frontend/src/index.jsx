2fdda3db87fb58db91ababc2834461ed0c435b46
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-07 00:56:16 -0400

    210

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -1,11 +1,12 @@
 import { FrontPage } from "./Front"
+import motion from 'motion/react'
 
 export function FullPage() {
     return (
         <>
         <FrontPage />
         <div className="size-full bg-slate-950/30">
-
+        <Link to='/users'><motion.h2 className="font-semibold text-[24px] text-blue-600/90" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>View Users</motion.h2></Link>
         </div>
         </>
     )
