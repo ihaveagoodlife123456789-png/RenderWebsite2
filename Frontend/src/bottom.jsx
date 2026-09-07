c5cbcee6d59a49b126a20d4c757ac6f1c0944f98
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-07 16:49:40 -0400

    256

--- Frontend/src/bottom.jsx
+++ Frontend/src/bottom.jsx
@@ -7,7 +7,7 @@ export function BottomPage() {
     return (
         <motion.div className="size-full bg-slate-950/30 flex justify-around items-center snap-start"> 
         <Link to='/users' className="overflow-hidden w-[20%] h-[48%] bg-mist-800/60 font-semibold text-[24px] text-blue-600/90 flex flex-col">
-        <motion.h2 className="size-full flex justify-center items-center bg-[url('/winter-mountain-5120x2880-24311.jpg')] bg-no-repeat bg-cover">
+        <motion.h2 className="size-screen flex justify-center items-center bg-[url('/winter-mountain-5120x2880-24311.jpg')] bg-no-repeat bg-cover">
         <h2 className="font-bold text-[42px] text-amber-600">View users</h2>
         </motion.h2>
         <motion.div className="w-full h-[35%] bg-mist-800/30 relative bottom-0">
