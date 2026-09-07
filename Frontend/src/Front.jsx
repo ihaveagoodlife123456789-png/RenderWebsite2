bf1235e2866a62482ab32f7c688aac862459dbe8
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-07 00:42:39 -0400

    209

--- Frontend/src/Front.jsx
+++ Frontend/src/Front.jsx
@@ -6,7 +6,7 @@ export function FrontPage() {
 
   return (
     <div className="size-full text-[35px] flex flex-col text-slate-200">
-      <div className="absolute flex items-center justify-center gap-5 text-white font-semibold text-[22px] top-12 w-[52%] h-[7%] left-[25%] bg-slate-600/70 rounded-[12px]">
+      <div className="fixed flex items-center justify-center gap-5 text-white font-semibold text-[22px] top-12 w-[52%] h-[7%] left-[25%] bg-slate-600/70 rounded-[12px]">
       <div className="size-fit absolute left-5 flex flex-col items-center justify-center">
       <motion.div className="text-[25px] font-bold text-indigo-800 border-b-0 border-indigo-600" initial={{scale: 0.3, y: -30}} animate={{scale: 1, y: 0}} transition={{duration: .1}} whileHover={{borderBottomWidth: '4px', pointer: 'cursor'}}>Ascended Horizons</motion.div>
       </div>
