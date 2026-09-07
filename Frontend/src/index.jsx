07a3baa91c8ead9d4bf01977a26f40717d65c81f
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-07 13:38:02 -0400

    232

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export function FullPage() {
 
     return (
-        <div className="h-full w-full overflow-y-scroll snap-y snap-manditory" drag="y" dragConstraints={{ top: 200, bottom: 0 }} dragElastic={0} dragMomentum={true}>
+        <div className="h-full w-full overflow-y-scroll snap-y snap-manditory" drag="y" dragConstraints={{ top: 600, bottom: 0 }} dragElastic={0} dragMomentum={true}>
         <FrontPage />
         <motion.div className="size-full bg-slate-950/30 flex justify-center items-center snap-end">
         <Link to='/users'><motion.h2 className="font-semibold text-[24px] text-blue-600/90" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>View Users</motion.h2></Link>
