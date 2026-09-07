f48cc6bd51d2481c947b649aa2b13f91ace2fbb7
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-06 20:06:41 -0400

    183

--- Frontend/src/Front.jsx
+++ Frontend/src/Front.jsx
@@ -7,20 +7,20 @@ export function FrontPage() {
   return (
     <div className="size-full text-[35px] bg-[url('/winter-mountain-5120x2880-24311.jpg')] bg-no-repeat bg-cover bg-center flex flex-col text-slate-200">
       <div className="absolute flex items-center justify-center gap-5 text-white font-semibold text-[22px] top-12 w-[52%] h-[7%] left-[25%] bg-slate-600/70 rounded-[12px]">
-      <h2 className="absolute left-5 text-[25px] font-bold text-indigo-600">Ascended Horizons</h2>
-      <h4>About</h4>
-      <h4>Company</h4>
-      <h4>Explore</h4>
-      <h4>Services</h4>
-      <h2 className="absolute right-5 text-[26px] text-violet-500">Sign In</h2>
+      <h2 className="absolute left-5 text-[25px] font-bold text-indigo-800 border-b-0 border-indigo-900/90" initial={{scale: .3, y: -30}} animate={{scale: 1, y: 0}} whileHover={{borderBottomWidth: '225px'}}>Ascended Horizons</h2>
+      <motion.h4 initial={{scale: 1, opacity: 0, color: 'white'}} animate={{ opacity: 1}} transition={{duration: .4}} whileHover={{ scale: 1.06, color: 'golden-orange'}}>About</motion.h4>
+      <motion.h4 initial={{scale: 1, opacity: 0, color: 'white'}} animate={{ opacity: 1}} transition={{duration: .4}} whileHover={{ scale: 1.06, color: 'golden-orange'}}>Company</motion.h4>
+      <motion.h4 initial={{scale: 1, opacity: 0, color: 'white'}} animate={{ opacity: 1}} transition={{duration: .4}} whileHover={{ scale: 1.06, color: 'golden-orange'}}>Explore</motion.h4>
+      <motion.h4 initial={{scale: 1, opacity: 0, color: 'white'}} animate={{ opacity: 1}} transition={{duration: .4}} whileHover={{ scale: 1.06, color: 'golden-orange'}}>Services</motion.h4>
+      <h2 className="absolute right-5 text-[26px] text-violet-500" initial={{scale: 1}} whileHover={{scale: 1.08, color: 'white'}}>Sign In</h2>
       </div>
-      <div className="absolute font-bold text-[26px] top-15 right-[10%] w-[5%] h-[5%] bg-blue-700 rounded-[13px] text-center text-white">Login</div>
-      <div className="w-full h-[82%] flex flex-col justify-center items-center gap-20">
+      <div className="absolute font-bold text-[26px] top-15 right-[10%] w-[5%] h-[5%] bg-blue-700 rounded-[13px] text-white" initial={{scale: 1}} whileHover={{scale: 1.08, color: '#1d4ed8', backgroundColor: 'white'}}>Login</div>
+      <div className="w-full h-[88%] flex flex-col justify-center items-center gap-20">
         <h1 className="font-bold text-[48px] text-orange-400/90">Ascended Horizons</h1>
         <h4 className="text-[24px] leading-[42px] font-semibold text-slate-950 text-center">Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. <br />Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. <br />Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</h4>
         <Link to='/form'><motion.h2 className="font-semibold text-[24px] py-2 px-4 text-white bg-blue-600/80 border-blue-700/70 border-[3px] rounded-[15px]" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>Write a Message</motion.h2></Link>
       </div>
-      <div className="absolute bottom-0 border-t-2 border-slate-800/70 w-full h-[18%] bg-slate-950/30 flex justify-center items-center gap-20 border-slate-400 border-[1px]">
+      <div className="absolute bottom-0 border-t-2 border-slate-800/70 w-full h-[12%] bg-slate-950/30 flex justify-center items-center gap-20 border-slate-400 border-[1px]">
         <Link to='/users'><motion.h2 className="font-semibold text-[24px] text-blue-600/90" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08}}>View Users</motion.h2></Link>
       </div>
       <img src="/icons8-wreath-64.png" className="absolute top-16 left-16 size-[100px]"/>
