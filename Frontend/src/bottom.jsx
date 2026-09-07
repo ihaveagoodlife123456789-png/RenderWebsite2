09cf7a477de26427cd79d7345dc09a43d0b46bdd
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-07 18:55:09 -0400

    260

--- Frontend/src/bottom.jsx
+++ Frontend/src/bottom.jsx
@@ -9,11 +9,11 @@ export function BottomPage() {
         
         <Link to='/users' className="w-[20%] h-[48%] bg-mist-800/60 font-semibold text-[24px] text-blue-600/90 flex flex-col">
 
-        <motion.h2 className="w-full h-[65%] flex justify-center items-center bg-[url('/rocky-mountains-3840x3840-26304.jpg')] bg-no-repeat bg-cover bg-center">
+        <motion.h2 className="w-full h-[65%] flex justify-center items-center bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf95GKxp-bYuHnEyd_3sSR21oAbE_l0AjFwxKGYQ0EUA&s=10')] bg-no-repeat bg-cover bg-center">
         <h2 className="font-bold text-[42px] text-amber-600">View users</h2>
         </motion.h2>
 
-        <motion.div className="w-full h-[35%] bg-mist-800/30 relative bottom-0 text-white"> Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.<br /> Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.</motion.div>
+        <motion.div className="w-full h-[35%] bg-mist-800/30 relative bottom-0 text-white text-[18px] text-center"> Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.<br /> Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.</motion.div>
 
         </Link>
 
