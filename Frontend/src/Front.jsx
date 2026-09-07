7963cf35865870b8079f02f57017954398eef86a
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-07 01:43:58 -0400

    219

--- Frontend/src/Front.jsx
+++ Frontend/src/Front.jsx
@@ -1,6 +1,6 @@
 
 import { Link } from 'react-router-dom';
-import { motion, useTransform, scrollYProgress, useScroll } from 'motion/react';
+import { motion, useTransform, useScroll } from 'motion/react';
 
 const { scrollYProgress } = useScroll()
 
@@ -13,7 +13,7 @@ const filter = useTransform(
 export function FrontPage() {
 
   return (
-    <div className="size-full text-[35px] flex flex-col text-slate-200">
+    <div className="size-full text-[35px] flex flex-col text-slate-200" style={{ filter }}>
       <div className="fixed flex items-center justify-center gap-5 text-white font-semibold text-[22px] top-12 w-[52%] h-[7%] left-[25%] bg-slate-600/70 rounded-[12px]">
       <div className="size-fit absolute left-5 flex flex-col items-center justify-center">
       <motion.div className="text-[25px] font-bold text-indigo-800 border-b-0 border-indigo-600" initial={{scale: 0.3, y: -30}} animate={{scale: 1, y: 0}} transition={{duration: .1}} whileHover={{borderBottomWidth: '4px', pointer: 'cursor'}}>Ascended Horizons</motion.div>
