423f36bce1b28178c4d6e7d15e552fa7284b31e1
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-06 23:42:29 -0400

    203

--- Frontend/src/Front.jsx
+++ Frontend/src/Front.jsx
@@ -5,7 +5,7 @@ import { motion } from 'motion/react';
 export function FrontPage() {
 
   return (
-    <div className="size-full text-[35px] bg-[url('/winter-mountain-5120x2880-24311.jpg')] bg-no-repeat bg-cover bg-center flex flex-col text-slate-200">
+    <div className="size-full overflow-auto text-[35px] bg-[url('/winter-mountain-5120x2880-24311.jpg')] bg-no-repeat bg-cover bg-center flex flex-col text-slate-200">
       <div className="absolute flex items-center justify-center gap-5 text-white font-semibold text-[22px] top-12 w-[52%] h-[7%] left-[25%] bg-slate-600/70 rounded-[12px]">
       <div className="size-fit absolute left-5 flex flex-col items-center justify-center">
       <motion.div className="text-[25px] font-bold text-indigo-800 border-b-0 border-indigo-600" initial={{scale: 0.3, y: -30}} animate={{scale: 1, y: 0}} transition={{duration: .1}} whileHover={{borderBottomWidth: '4px', pointer: 'cursor'}}>Ascended Horizons</motion.div>
--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -4,7 +4,7 @@ export function FullPage() {
     return (
         <>
         <FrontPage />
-        <div className="size-full bg-slate-950/30">
+        <div className="size-full bg-slate-950/30 overflow-auto">
 
         </div>
         </>
