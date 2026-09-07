6237b7b34a742342c31cd698006ae2e7c49eb536
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-06 23:51:05 -0400

    204

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -4,7 +4,7 @@ export function FullPage() {
     return (
         <>
         <FrontPage />
-        <div className="size-full bg-slate-950/30 overflow-auto">
+        <div className="size-full bg-slate-950/30 overflow-auto scrollbar-none">
 
         </div>
         </>
