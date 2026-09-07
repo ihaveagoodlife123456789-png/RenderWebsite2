e76e581b4d338b9291ae5472e8b22533d1e7c52b
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-06 23:40:42 -0400

    201

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -2,11 +2,11 @@ import { FrontPage } from "./Front"
 
 export function FullPage() {
     return (
-        <>
+        <div className="overflow-auto">
         <FrontPage />
         <div className="size-full bg-slate-950/30">
 
         </div>
-        </>
+        </div>
     )
 }
