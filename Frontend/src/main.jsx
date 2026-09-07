b09c81b0b934f47d215ff1a8d05f295ba4764996
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-06 23:31:55 -0400

    200

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { FullFrontPage } from './index.jsx'
+import { FullPage } from './index.jsx'
 import { FormPage } from './form.jsx'
 import { UsersPage } from './users.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <FullFrontPage />,
+    element: <FullPage />,
   },
   {
     path: '/form',
