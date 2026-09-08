d8a751042fec2884b8b4a3702ca25734dc31f5da
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-08 01:05:36 -0400

    315

--- Frontend/src/users.jsx
+++ Frontend/src/users.jsx
@@ -46,7 +46,7 @@ export function UsersPage() {
 
 
                 <h1 className="absolute size-fit top-8 text-[35px] text-white font-bold size-[35px]">Messages</h1>
-                <Link to='/' className="absolute bottom-12"><motion.h2 className="bg-blue-400/20 border-[2px] border-blue-700 text-[22px] font-bold py-[6px] px-[8px] rounded-[12px]" initial={{scale: 1}} whileHover={{scale: 1.08}}>Go back</motion.h2></Link>
+                <Link to='/' className="absolute bottom-12"><motion.h2 className="bg-blue-400/70 border-[2px] border-blue-700 text-[22px] font-bold py-[6px] px-[8px] rounded-[12px]" initial={{scale: 1}} whileHover={{scale: 1.08}}>Go back</motion.h2></Link>
             </div>
 
 
