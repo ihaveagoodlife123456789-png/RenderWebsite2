e3b01dd19327bffd62a81e5ae488205490d3c695
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-07 21:36:59 -0400

    265

--- Frontend/src/bottom.jsx
+++ Frontend/src/bottom.jsx
@@ -1,16 +1,33 @@
 import { motion } from 'motion/react'
 import { Link } from 'react-router-dom';
-
+import { useState } from 'react'
 
 
 export function BottomPage() {
+    const [animate, setAnimate] = useState(false);
+
+    function onMouseEnterAnimation() {
+        setAnimate(!animate)
+    }
+
+    const variants = {
+        on: {opacity: 1},
+        off: {opacity: 0},
+    }
     return (
         <motion.div className="size-full bg-slate-950/30 flex justify-around items-center snap-start">
         
-        <Link to='/users' className="w-[20%] h-[48%] bg-mist-800/60 font-semibold text-[24px] text-blue-600/90 flex flex-col">
+        <Link to='/users' className="w-[20%] h-[48%] bg-mist-800/60 font-semibold text-[24px] text-blue-600/90 flex flex-col"
+        onMouseEnter={() => onMouseEnterAnimation()}
+        >
 
         <motion.h2 className="w-full h-[65%] flex justify-center items-center bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf95GKxp-bYuHnEyd_3sSR21oAbE_l0AjFwxKGYQ0EUA&s=10')] bg-no-repeat bg-cover bg-center">
-        <h2 className="font-bold text-[42px] text-amber-600">View users</h2>
+        <h2 className="font-bold text-[42px] text-amber-600"
+        variants={variants}
+        initial={{opacity: 1}}
+        transition={{duration: .5}}
+        animate={animate ? 'on' : 'off'}
+        >View users</h2>
         </motion.h2>
 
         <motion.div className="w-full h-[35%] bg-mist-800/30 relative bottom-0 text-white text-[12px] text-center font-bold flex flex-col items-center justify-center">
