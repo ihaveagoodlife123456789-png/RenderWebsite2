0e0771d94e8c4ec3fe2301d2f7b2247121029c38
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-08 00:26:31 -0400

    301

--- Frontend/src/top.jsx
+++ Frontend/src/top.jsx
@@ -43,8 +43,8 @@ export function TopPage() {
         <Link to='/form'><motion.h2 className="font-semibold text-[24px] py-2 px-4 text-white bg-blue-600/80 border-blue-700/70 border-[3px] rounded-[15px]" initial={{ opacity: 0, y: -20, color: 'white', backgroundColor: '#2563eb', borderColor: '#1d4ed8' }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08, color: '#2563eb', backgroundColor: 'white', borderColor: 'none'}}>Write a Message</motion.h2></Link>
       </div>
       <div className="relative bottom-0 border-t-2 border-slate-800/70 w-full h-[12%] bg-slate-950/30 flex flex-col justify-center items-center gap-2">
-        <h2 className="text-[24px] text-white">More Here</h2>
-        <motion.a href="#top" onClick={scroll} style={{rotate: '90deg'}} initial={{y: 0}} animate={{y: [0, 10, 0, -10, 0]}} transition={{duration: 2, repeat: Infinity, ease: 'linear'}}>{'>'}</motion.a>
+        <a href={`#${services}`} onClick={scrollServices} className="text-[24px] text-white">More Here</a>
+        <motion.a href={`#${services}`} onClick={scrollServices} style={{rotate: '90deg'}} initial={{y: 0}} animate={{y: [0, 10, 0, -10, 0]}} transition={{duration: 2, repeat: Infinity, ease: 'linear'}}>{'>'}</motion.a>
       </div>
       <img src="/icons8-wreath-64.png" className="fixed top-16 left-16 size-[100px]"/>
     </motion.div>
