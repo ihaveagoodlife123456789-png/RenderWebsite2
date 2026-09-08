7be14e85a466f83cd612602b0920b1edd62bfcc2
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-07 21:45:30 -0400

    267

--- Frontend/src/bottom.jsx
+++ Frontend/src/bottom.jsx
@@ -11,14 +11,17 @@ export function BottomPage() {
     }
 
     const variants = {
-        on: {opacity: 0},
-        off: {opacity: 1},
+        Oon: {opacity: 0},
+        Ooff: {opacity: 1},
+        Hon: {scale: 0},
+        Hoof: {scale: 1},
     }
     return (
         <motion.div className="size-full bg-slate-950/30 flex justify-around items-center snap-start">
         
         <Link to='/users' className="w-[20%] h-[48%] bg-mist-800/60 font-semibold text-[24px] text-blue-600/90 flex flex-col"
         onMouseEnter={() => onMouseEnterAnimation()}
+        onMouseLeave={() => onMouseEnterAnimation()}
         >
 
         <motion.h2 className="w-full h-[65%] flex justify-center items-center bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf95GKxp-bYuHnEyd_3sSR21oAbE_l0AjFwxKGYQ0EUA&s=10')] bg-no-repeat bg-cover bg-center">
@@ -26,11 +29,16 @@ export function BottomPage() {
         variants={variants}
         initial={{opacity: 1}}
         transition={{duration: .5}}
-        animate={animate ? 'on' : 'off'}
+        animate={animate ? 'Oon' : 'Ooff'}
         >View users</motion.h2>
         </motion.h2>
 
-        <motion.div className="w-full h-[35%] bg-mist-800/30 relative bottom-0 text-white text-[12px] text-center font-bold flex flex-col items-center justify-center">
+        <motion.div className="w-full h-[35%] bg-mist-800/30 relative bottom-0 text-white text-[12px] text-center font-bold flex flex-col items-center justify-center"
+        variants={variants}
+        initial={{scale: 1}}
+        transition={{duration: .5}}
+        animate={animate ? 'Hon' : 'Hoof'}
+        >
         <h2 className="font-semibold text-[28px]">Liebestrum</h2>
         <h3 className="w-[70%]">Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.<br /> Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.</h3>
         </motion.div>
