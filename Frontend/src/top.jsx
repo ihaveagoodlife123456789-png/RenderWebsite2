c32f4b728b6dfba421b8ea515f007fb947475245
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-08 22:41:12 -0400

    339

--- Frontend/src/top.jsx
+++ Frontend/src/top.jsx
@@ -34,7 +34,7 @@ export function TopPage() {
         transition={{ duration: 0.5 }}
         viewport={{ once: false }}
     >
-      <div className="fixed flex items-center justify-center gap-5 text-white font-semibold sm:text-[12px] md:text-[14px] lg:text-[16px] xl:text-[18px] 2xl:text-[22px] top-12 w-[52%] h-[7%] left-[25%] bg-slate-600/70 rounded-[12px]">
+      <div className="fixed flex items-center justify-center gap-5 text-white font-semibold sm:text-[12px] md:text-[14px] lg:text-[16px] xl:text-[18px] 2xl:text-[22px] top-12 w-[52%] h-[7%] left-[25%] bg-slate-600/70 rounded-[12px] md:left-[10%] md:h-[70%] md:h-[12%] md:flex-col md:gap-10">
       <div className="size-fit flex flex-col items-center justify-center">
       <motion.a href="#top" onClick={scroll} className="sm:text-[14px] mr-[20px] md:text-[16px] lg:text-[18px] xl:text-[20px] 2xl:text-[24px] font-bold text-indigo-800 border-b-0 border-indigo-600" initial={{scale: 0.3, y: -30}} animate={{scale: 1, y: 0}} transition={{duration: .1}} whileHover={{borderBottomWidth: '4px', pointer: 'cursor'}}>Ascended Horizons</motion.a>
       </div>
