a54f58e03fcb0434d84050f3d8232760eebb262b
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-08 22:34:00 -0400

    338

--- Frontend/src/top.jsx
+++ Frontend/src/top.jsx
@@ -36,7 +36,7 @@ export function TopPage() {
     >
       <div className="fixed flex items-center justify-center gap-5 text-white font-semibold sm:text-[12px] md:text-[14px] lg:text-[16px] xl:text-[18px] 2xl:text-[22px] top-12 w-[52%] h-[7%] left-[25%] bg-slate-600/70 rounded-[12px]">
       <div className="size-fit flex flex-col items-center justify-center">
-      <motion.a href="#top" onClick={scroll} className="sm:text-[14px] mr-[15px] md:text-[16px] lg:text-[18px] xl:text-[20px] 2xl:text-[24px] font-bold text-indigo-800 border-b-0 border-indigo-600" initial={{scale: 0.3, y: -30}} animate={{scale: 1, y: 0}} transition={{duration: .1}} whileHover={{borderBottomWidth: '4px', pointer: 'cursor'}}>Ascended Horizons</motion.a>
+      <motion.a href="#top" onClick={scroll} className="sm:text-[14px] mr-[20px] md:text-[16px] lg:text-[18px] xl:text-[20px] 2xl:text-[24px] font-bold text-indigo-800 border-b-0 border-indigo-600" initial={{scale: 0.3, y: -30}} animate={{scale: 1, y: 0}} transition={{duration: .1}} whileHover={{borderBottomWidth: '4px', pointer: 'cursor'}}>Ascended Horizons</motion.a>
       </div>
       <motion.h4 className="size-fit" initial={{scale: 1, opacity: 0, color: 'white'}} animate={{ opacity: 1}} whileHover={{ scale: 1.08, color: 'orange', pointer: 'cursor'}}>About</motion.h4>
       <motion.h4 className="size-fit" initial={{scale: 1, opacity: 0, color: 'white'}} animate={{ opacity: 1}} whileHover={{ scale: 1.08, color: 'orange', pointer: 'cursor'}}>Company</motion.h4>
@@ -44,7 +44,7 @@ export function TopPage() {
       <motion.a className="size-fit" initial={{scale: 1, opacity: 0, color: 'white'}} animate={{ opacity: 1}} whileHover={{ scale: 1.08, color: 'orange', pointer: 'cursor'}}>Services</motion.a>
       <motion.h2 className="size-fit sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[20px] 2xl:text-[24px] text-violet-500 ml-[15px]" initial={{scale: 1}} whileHover={{scale: 1.08, color: 'white', pointer: 'cursor'}}>Sign In</motion.h2>
       </div>
-      <motion.div className="size-fit py-2 px-3 fixed font-bold text-[26px] top-15 sm:right-[4%] md:right-[5%] lg:right-[6%] xl:right-[8%] 2xl:right-[10%] bg-blue-700 rounded-[13px] text-white " initial={{scale: 1, color:'white', backgroundColor: '#1d4ed8'}} whileHover={{scale: 1.08, color: '#1d4ed8', backgroundColor: 'white', pointer: 'cursor'}}>Login</motion.div>
+      <motion.div className="size-fit py-2 px-3 fixed font-bold top-15 sm:right-[4%] md:right-[5%] lg:right-[6%] xl:right-[8%] 2xl:right-[10%] bg-blue-700 rounded-[13px] text-white sm:text-[20px] lg:text-[23px] 2xl:text-[26px] md:py-1 md:px-2" initial={{scale: 1, color:'white', backgroundColor: '#1d4ed8'}} whileHover={{scale: 1.08, color: '#1d4ed8', backgroundColor: 'white', pointer: 'cursor'}}>Login</motion.div>
       <div className="w-full h-[88%] flex flex-col justify-center items-center gap-20">
         <h1 className="font-bold sm:text-[32px] md:text-[36px] lg:text-[40px] xl:text-[44px] 2xl:text-[48px] text-orange-400/90">Ascended Horizons</h1>
         <h4 className="text-sm sm:font-serif sm:text-base md:text-lg lg:text-xl leading-[42px] font-semibold text-slate-950 text-center">Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. <br />Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. <br />Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</h4>
@@ -54,7 +54,7 @@ export function TopPage() {
         <a href="#explore" onClick={scrollExplore} className="text-[24px] text-white">More Here</a>
         <motion.a href="#explore" onClick={scrollExplore} style={{rotate: '90deg'}} initial={{y: 0}} animate={{y: [0, 10, 0, -10, 0]}} transition={{duration: 2, repeat: Infinity, ease: 'linear'}}>{'>'}</motion.a>
       </div>
-      <img src="/icons8-wreath-64.png" className="fixed top-8 sm:left-9 md:left-10 lg:left-12 xl:left-14 2xl:left-16 size-[100px]"/>
+      <img src="/icons8-wreath-64.png" className="fixed top-8 sm:left-9 md:left-10 lg:left-12 xl:left-14 2xl:left-16 sm:size-[55px] md:size-[65px] lg:size-[75px] xl:size-[85px] 2xl:size-[100px]"/>
     </motion.div>
   )
 }
