b21b0213e542178124673064c38f053eda401293
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-11 17:54:43 -0400

    410

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.use(
         cookie: {
             domain: 'ascendedhorizons.com',
             httpOnly: true,
-            secure: req.headers['x-forwarded-proto'] === 'https',
+            secure: false,
             sameSite: 'lax',
             maxAge: 1000 * 60 * 5
         }
