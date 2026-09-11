9da289b7d130e918c8aa4c1f3ae137fab5dfbe43
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-11 18:13:18 -0400

    415

--- Frontend/src/signIn.jsx
+++ Frontend/src/signIn.jsx
@@ -30,6 +30,7 @@ const {
                 headers: {
                     'Content-Type': 'application/json'
                 },
+                credentials: 'include'
                 body: JSON.stringify(data)
             })
 
