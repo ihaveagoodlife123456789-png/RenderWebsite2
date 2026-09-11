89ada43d8b65186a2eba356c5b5928d135b44307
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-11 19:03:39 -0400

    419

--- Backend/server.js
+++ Backend/server.js
@@ -43,7 +43,7 @@ app.use(
 app.post('/api/signIn', async (req, res) => {
     const {username, password} = req.body;
     try {
-        if(typeof password === 'string') {
+        if(typeof password === 'number') {
            return res.status(401).send({message: 'Password must be a number'})
     }
 
