93a860ef956598df07451b86b461a12a05360140
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-10 22:51:18 -0400

    395

--- Frontend/src/signIn.jsx
+++ Frontend/src/signIn.jsx
@@ -52,7 +52,7 @@ const {
     return (
         <div className="size-full bg-[url('/snowy-village-5120x2880-20406.jpg')] bg-no-repeat bg-cover flex justify-center items-center">
             <Toaster position="top-right" toastOptions={{style: {background: 'green', color: 'white'}}} />
-            <div className="size-[70%] bg-slate-950/60 flex justify-center items-center">
+            <div className="size-[70%] bg-slate-950/60 flex justify-center items-center flex-col gap-12 text-white font-bold text-[25px]">
                 <form onSubmit={handleSubmit(loginSubmit)} className="size-full flex justify-center items-center flex-col">
                     <h2 className="text-white font-bold">Username</h2>
                     <input {...register('username')} type="text" placeholder='username' className="border-[3px] border-slate-950/90"></input>
