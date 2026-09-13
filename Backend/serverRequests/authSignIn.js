d05e8e83e5a663732df06f76091e375c5949849e
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-13 15:26:33 -0400

    590

--- Backend/serverRequests/authSignIn.js
+++ Backend/serverRequests/authSignIn.js
@@ -9,12 +9,12 @@ authSignIn.post('/', async (req, res) => {
     const {username, password, email} = req.body
     try {
         const verify = `SELECT * FROM authenticate WHERE email = $1`
-        const verifyEmail = await pool.query(verify, [email])
+        const verifyEmail = await pool.query(verify, [email.toLowerCase()])
         if(verifyEmail.rows.length === 1) {
             return res.status(401).json({message: 'A user with this email already exists.'})
         }
         const query = `INSERT INTO authenticate (username, password, email) VALUES ($1, $2, $3)`
-        const values = [username.toLowerCase(), password, email]
+        const values = [username.toLowerCase(), password, email.toLowerCase()]
         const result = await pool.query(query, values)
         return res.status(201).json(result)
     } catch (err) {
