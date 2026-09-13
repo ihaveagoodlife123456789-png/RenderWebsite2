4e7280c933a7a45f2b4f692f59209fcd95ccf4f5
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-12 22:40:18 -0400

    449

--- Frontend/src/profiles.jsx
+++ Frontend/src/profiles.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { motion } from 'motion/react'
 
 export function GetUserProfile() {
     const [userData, setUserData] = useState(null)
+    const [logError, setLogError] = useState(null)
     useEffect(() => {
         async function getUser() {
             try  {
@@ -13,6 +15,7 @@ export function GetUserProfile() {
                 )
                 if(!getResponse) {
                     console.log(getResponse.message)
+                    setLogError(getResponse.message.message)
                     throw new Error({message: getResponse.message})
                 }
                 const data = await getResponse.json()
@@ -25,17 +28,19 @@ export function GetUserProfile() {
         getUser()
     }, [])
     return (
-        <div className="text-white text-[24px] font-bold size-full bg-[url('/snowy-village-5120x2880-20406.jpg')] bg-no-repeat bg-cover flex justify-center items-center">
-            <div className="size-[70%] bg-slate-950/60 flex flex-col justify-center items-center">
+        <div className="text-blue-800 text-[24px] font-bold size-full bg-[url('/snowy-village-5120x2880-20406.jpg')] bg-no-repeat bg-cover flex justify-center items-center">
+            <div className="ext-blue-800 size-[70%] bg-slate-950/60 flex flex-col justify-center items-center">
             {userData ? (
                 <>
-                <h1 className="text-white font-bold">{`username: ${userData.username}`}</h1>
-                    <h1 className="text-white font-bold">{`password: ${userData.password}`}</h1>
+                <h1 className="text-white font-bold">{userData.username ? `Username: ${userData.username}` : 'Na'}</h1>
+                    <h1 className="text-white font-bold">{userData.password ? `Password: ${userData.password}` : 'Na'}</h1>
+                    {logError ? <h2 className="text-red-700 font-bold text-[24px]">{logError}</h2> : null}
                     </>
             ) : (
                 <div>Loading...</div>
             )}
-            <Link to="/signIn">Sign In</Link>
+            <Link to="/signIn" className="relative top-3 size-fit bg-lime-600"><motion.div initial={{scale: 1, color: 'white', backgroundColor: '#5EA500'}} whileHover={{scale: 1.06, color: '#5EA500', backgroundColor: 'white'}}>Sign In</motion.div></Link>
+                    <Link to="/" className="absolute top-2 left-2 size-fit bg-blu-800"><motion.div initial={{scale: 1}} whileHover={{scale: 1.05}}>Home</motion.div></Link>
             </div>
         </div>
     )
