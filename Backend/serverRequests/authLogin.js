b4ea5ff05134b71a35850bb660dc19165232780f
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-12 23:45:04 -0400

    561

--- Backend/serverRequests/authLogin.js
+++ Backend/serverRequests/authLogin.js
@@ -4,6 +4,21 @@ import passport from 'passport';
 
 export const authLoginRouter = express.Router()
 
-authLoginRouter.post('/', passport.authenticate("local", { failureRedirect: '/login' }), (req, res) => {
-    res.json({ message: 'Logged in Successfully'})
+authLoginRouter.post('/', (req, res) => {
+    passport.authenticate("local", (err, user, info) => {
+        if (err) {
+            return next(err)
+        }
+
+        if (!user) {
+            return res.status(401).json({ error: info.message})
+        }
+
+        req.logIn(user, (err) => {
+            if (err) {
+                return(err);
+            }
+            return res.json({ message: 'Logged in Successfully', user: user})
+        })
+    })
 })
