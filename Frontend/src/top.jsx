c8692a5e341197a001d4d897d4b72bc2bbf82f0b
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-13 00:38:42 -0400

    571

--- Frontend/src/top.jsx
+++ Frontend/src/top.jsx
@@ -7,7 +7,7 @@ import { logout } from './logout.js'
 import { Toaster } from 'sonner'
 
 export function TopPage() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(false)
 
   const ref = useRef(null)
 
@@ -36,7 +36,7 @@ useEffect(() => {
   const response = await fetch(url)
   const result = await response.json()
   if(!result) {
-    setUser(null)
+    setUser(false)
     return;
   }
   console.log(result.username)
@@ -56,7 +56,7 @@ useEffect(() => {
        <Toaster position="bottom-right" toastOptions={{style: {background: 'green', color: 'white'}}} />
       <div className="fixed flex items-center justify-center gap-5 text-white font-semibold sm:text-[12px] md:text-[14px] lg:text-[16px] xl:text-[18px] 2xl:text-[22px] top-12 w-[52%] h-[7%] left-[25%] bg-slate-600/70 rounded-[12px]">
       <div className="size-fit flex flex-col items-center justify-center">
-      <motion.a href="#top" onClick={scroll} className="sm:text-[14px] mr-[20px] md:text-[16px] lg:text-[18px] xl:text-[20px] 2xl:text-[24px] font-bold text-indigo-800 border-b-0 border-indigo-600" initial={{scale: 0.3, y: -30}} animate={{scale: 1, y: 0}} transition={{duration: .1}} whileHover={{borderBottomWidth: '4px', pointer: 'cursor'}}>{user ? <h1 className="text-red-800">{`Welcome back ${user.username}`}</h1> : 'Ascended Horizons'}</motion.a>
+      <motion.a href="#top" onClick={scroll} className="sm:text-[14px] mr-[20px] md:text-[16px] lg:text-[18px] xl:text-[20px] 2xl:text-[24px] font-bold text-indigo-800 border-b-0 border-indigo-600" initial={{scale: 0.3, y: -30}} animate={{scale: 1, y: 0}} transition={{duration: .1}} whileHover={{borderBottomWidth: '4px', pointer: 'cursor'}}>Ascended Horizons</motion.a>
       </div>
       <motion.h4 className="size-fit" initial={{scale: 1, opacity: 0, color: 'white'}} animate={{ opacity: 1}} whileHover={{ scale: 1.08, color: 'orange', pointer: 'cursor'}}>About</motion.h4>
       <motion.h4 className="size-fit" initial={{scale: 1, opacity: 0, color: 'white'}} animate={{ opacity: 1}} whileHover={{ scale: 1.08, color: 'orange', pointer: 'cursor'}}>Company</motion.h4>
@@ -66,7 +66,7 @@ useEffect(() => {
       </div>
       <Link to="/login" className="size-fit py-2 px-3 fixed font-bold top-15 sm:right-[4%] md:right-[5%] lg:right-[6%] xl:right-[8%] 2xl:right-[10%] bg-blue-700 rounded-[13px] text-white sm:text-[20px] lg:text-[23px] 2xl:text-[26px] md:py-1 md:px-2"><motion.div initial={{scale: 1, color:'white', backgroundColor: '#1d4ed8'}} whileHover={{scale: 1.08, color: '#1d4ed8', backgroundColor: 'white', pointer: 'cursor'}}>Login</motion.div></Link>
       <div className="w-full h-[88%] flex flex-col justify-center items-center gap-20">
-        <h1 className="font-bold sm:text-[32px] md:text-[36px] lg:text-[40px] xl:text-[44px] 2xl:text-[48px] text-orange-400/90">Ascended Horizons</h1>
+        <h1 className="font-bold sm:text-[32px] md:text-[36px] lg:text-[40px] xl:text-[44px] 2xl:text-[48px] text-orange-400/90">{user ? <h1 className="text-lime-800">{`Welcome back ${user.username}`}</h1> : 'Ascended Horizons'}</h1>
         <h4 className="text-sm sm:font-serif sm:text-[17px] md:text-[20px] lg:text-[24px] leading-[42px] font-semibold text-slate-950 text-center">Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. <br />Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. <br />Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</h4>
         <Link to='/form'><motion.h2 className="font-semibold sm:text-[18px] md:text-[19px] lg:text-[20px] xl:text-[22px] 2xl:text-[24px] py-2 px-4 text-white bg-blue-600/80 border-blue-700/70 border-[3px] rounded-[15px]" initial={{ opacity: 0, y: -20, color: 'white', backgroundColor: '#2563eb', borderColor: '#1d4ed8' }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} whileHover={{ scale: 1.08, color: '#2563eb', backgroundColor: 'white', borderColor: 'none'}}>Write a Message</motion.h2></Link>
       </div>
