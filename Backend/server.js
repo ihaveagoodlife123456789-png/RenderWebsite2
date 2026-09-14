e4105a87cf43a920b0a44aa42c65d2a61834fcbe
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-14 17:02:41 -0400

    598

--- Backend/server.js
+++ Backend/server.js
@@ -90,7 +90,7 @@ passport.use(new GoogleStrategy(
     {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'https://ascendedhorizons.com'
+        callbackURL: 'https://ascendedhorizons.com/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
         console.log('User from Goggle:', profile)
