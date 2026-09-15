c615ac48f0499ab67e51db2ae5d3a488d9ee76e9
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-15 00:49:22 -0400

    624

--- Frontend/src/dashboard.jsx
+++ Frontend/src/dashboard.jsx
@@ -40,8 +40,8 @@ export function Dashboard() {
     return (
         <div className="size-full bg-[url('/snowy-village-5120x2880-20406.jpg')] bg-no-repeat bg-cover flex justify-center items-center">
             <div className="relative size-[86%] bg-slate-950/40 flex flex-col justify-center items-center">
-            {token ? <h2 className="text-red-800 font-semibold text-[26px]">You're logged in! Token: {user}</h2> : <h1 className="text-gree-700 font-semibold text-[28px]">Loading...</h1>}
-            {token ? <button onClick={() => handleLogout} className="text-white bg-red-700 py-1 px-2 rounded-[12px]">Logout</button> : null}
+            {token ? <h2 className="text-red-800 font-semibold text-[26px]">You're logged in! Token: {user.email}</h2> : <h1 className="text-gree-700 font-semibold text-[28px]">Loading...</h1>}
+            {token ? <button onClick={() => handleLogout()} className="text-white bg-red-700 py-1 px-2 rounded-[12px]">Logout</button> : null}
             <Link to="/" className="bg-blue-700 py-1 px-2 rounded-[12px] text-[24px] font-semibold absolute bottom-[12%]"><motion.h2 initial={{scale: 1}} whileHover={{scale: 1.06}}>Home</motion.h2></Link>
             </div>
         </div>
