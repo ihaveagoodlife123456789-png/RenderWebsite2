fd84c62abff9870a2ece5992e1656099d1d76623
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-14 22:43:02 -0400

    615

--- Backend/server.js
+++ Backend/server.js
@@ -66,7 +66,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser((user, done) => {
-    done(null, user.google_id)
+    done(null, user.id)
 })
 
 passport.deserializeUser(async (id, done) => {
