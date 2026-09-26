7c99cfac2a1c3e55a7b9655b91186433633bd8f0
Vincent <ihaveagoodlife123456789@gmail.com>
2026-09-26 00:54:58 -0400

    697

--- Frontend/src/paymentPage.jsx
+++ Frontend/src/paymentPage.jsx
@@ -4,7 +4,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { CheckoutForm } from './CheckoutForm'; // Import your form component
 
 // EXCLUSIVELY use your Publishable Key here (pk_test_...)
-const stripePromise = loadStripe('sk_test_51UHxV6H6cQDkkM4jpRwzbOsrkiC7HQ9P4DJk4pK6a8Kqy9fgEDbugv0feVwYAk0lxv3EM4eUNQHEWhT8SkC0YjmM00w8sJDDsL');
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_SECRET_KEY);
 
 export function PaymentPage() {
   const [clientSecret, setClientSecret] = useState(null);
